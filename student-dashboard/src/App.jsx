24b1093aca48a172ca00d1da65be3979594a9d60
2500031032 <ankushalonsoo@gmail.com>
2026-09-19 04:39:25 +0000

    Deploying

--- student-dashboard/src/App.jsx
+++ student-dashboard/src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1>Welcome to student dashboard</h1>
+      
+    <header>
+        <h1> Student DashBoard</h1>
+        <p>Welcome to student portal</p>
+    </header>
+
+    <main className='grid'>
+      <div className='card'>
+        <h2>Attendace</h2>
+        <p>92%</p>
+      </div>
+
+        <div className='card'>
+        <h2>CGPA</h2>
+        <p>8.95</p>
+      </div>
+
+
+        <div className='card'>
+        <h2>Courses</h2>
+        <p>6</p>
+      </div>
+
+    </main>
+
+    <footer>
+      <p>© 2026 Student Portal</p>
+    </footer>
+
 
     </>
   )
